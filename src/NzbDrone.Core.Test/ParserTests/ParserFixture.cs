458e0331e738dad3f66c2c1b5167fe03d02df321
assada <assada.ua@gmail.com>
2025-07-27 17:37:56 +0200

    skip test

--- src/NzbDrone.Core.Test/ParserTests/ParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/ParserFixture.cs
@@ -94,15 +94,16 @@ namespace NzbDrone.Core.Test.ParserTests
             result.ReleaseTitle.Should().Be(releaseTitle);
         }
 
-        // [TestCase("Босх: Спадок (S2E1) / Series: Legacy (S2E1) (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Босх: Спадок", "Series: Legacy")]
-        // [TestCase("Босх: Спадок / Series: Legacy / S2E1-4 of 10 (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Босх: Спадок", "Series: Legacy")]
-        // [TestCase("Босх: Спадок AKA Series: Legacy S02 1080p NF WEB-DL Dual- Audio DD+ 5.1 Atmos H.264-APEX", "Босх: Спадок", "Series: Legacy")]
-        // [TestCase("Босх.Спадок.AKA.Series.Legacy.S02.1080p.NF.WEB-DL.DUAL.DDP5.1.Atmos.H.264-APEX", "Босх Спадок", "Series Legacy")]
-        // public void should_parse_multiple_series_titles(string postTitle, params string[] titles)
-        // {
-        //     var seriesTitleInfo = Parser.Parser.ParseTitle(postTitle).SeriesTitleInfo;
-        //     seriesTitleInfo.AllTitles.Should().BeEquivalentTo(titles);
-        // }
+        [TestCase("Босх: Спадок (S2E1) / Series: Legacy (S2E1) (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Босх: Спадок", "Series: Legacy")]
+        [TestCase("Босх: Спадок / Series: Legacy / S2E1-4 of 10 (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Босх: Спадок", "Series: Legacy")]
+        [TestCase("Босх: Спадок AKA Series: Legacy S02 1080p NF WEB-DL Dual- Audio DD+ 5.1 Atmos H.264-APEX", "Босх: Спадок", "Series: Legacy")]
+        [TestCase("Босх.Спадок.AKA.Series.Legacy.S02.1080p.NF.WEB-DL.DUAL.DDP5.1.Atmos.H.264-APEX", "Босх Спадок", "Series Legacy")]
+        [Explicit]
+        public void should_parse_multiple_series_titles(string postTitle, params string[] titles)
+        {
+            var seriesTitleInfo = Parser.Parser.ParseTitle(postTitle).SeriesTitleInfo;
+            seriesTitleInfo.AllTitles.Should().BeEquivalentTo(titles);
+        }
 
         [TestCase("[Reza] Series in Russian - S01E08 [WEBRip 1080p HEVC AAC] (Dual Audio) (Tokidoki Bosotto Russiago de Dereru Tonari no Alya-san)", "Unknown")]
         public void should_parse_language_after_parsing_title(string postTitle, string expectedLanguage)
